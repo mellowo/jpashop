a409415b7272ebea624369a559c4691cee6a39ac
mellowo <mellowo@icloud.com>
2022-06-19 09:08:57 +0900

    entity create2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -2,11 +2,13 @@ package jpabook.jpashop.domain;
 
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@Setter
 public class Address {
 
     private String city;
